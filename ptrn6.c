8e760ede9c7c012d70e08e94d42370bc32b30c15
Gani25 <memonabdulgani3d@gmail.com>
2025-07-16 18:21:53 +0530

    Patterns

---
+++ ptrn6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // ROW
+    for (i = 1; i <= n; i++)
+    {
+        // COLUMN (REVERSE)
+        for (j = n; j >= 1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
