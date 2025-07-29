58c74b5c8cda1f12ba0cfc73abb0b9e7434b85f9
Gani25 <memonabdulgani3d@gmail.com>
2025-07-29 18:39:24 +0530

    Sorting In Desc

--- arr11.c
+++ arr11.c
@@ -1,10 +1,12 @@
+// SORT given array in descending order
+// {8,50,6,-9,25}
 // WAP to sort given array
 // Bubble sort
 
 #include <stdio.h>
 int main()
 {
-    int arr[] = {30, 25, 10, 8, -9, -35, -60};
+    int arr[] = {8, 50, 6, -9, 25};
 
     int i, j;
     int size = sizeof(arr) / sizeof(int);
@@ -21,11 +23,11 @@ int main()
     // NUMBER OF PASSES
     for (i = 0; i < size; i++)
     {
-        // In each pass we have to compare current element from next element
-        // i = 0, 7 - 0 - 1 = 6 ->j = 0 1 2 3 4 5
+
         for (j = 0; j < size - i - 1; j++)
         {
-            if (arr[j] > arr[j + 1])
+            // DESC first element should be less than second element
+            if (arr[j] < arr[j + 1])
             {
                 // SWAP
                 int temp = arr[j];
@@ -42,14 +44,3 @@ int main()
         printf("%d\t", arr[i]);
     }
 }
-
-// Check whether the given array is sorted or not take input in array from user
-// size of array is fixed i.e 5
-
-// input 1: 1,3,9,12,20
-// ouput - Given array 1 3 9 12 20 is sorted array
-
-// input 2: 2,20,6,8,-5
-// ouput - Given array 2,20,6,8,-5 is not sorted array
-
-// Dipchi, Ayush
