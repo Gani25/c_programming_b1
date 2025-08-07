ae45bd0692fea0459c6495ff28cd12586fc2aead
Gani25 <memonabdulgani3d@gmail.com>
2025-08-07 18:03:56 +0530

    Functions Intro

---
+++ fn1.c
@@ -0,0 +1,27 @@
+// WAP to add 2 numbers -> perform this 3 times
+#include <stdio.h>
+
+// without return type - void, without parameters (empty)
+// function definition {}
+void addition()
+{
+    int n1, n2;
+    int result;
+    printf("Enter 2 numbers: ");
+    scanf("%d%d", &n1, &n2);
+
+    result = n1 + n2;
+
+    printf("Addition of %d, %d = %d\n", n1, n2, result);
+}
+
+int main()
+{
+    // Fn call
+    addition();
+
+    addition();
+    addition();
+}
+// WAP to create function which display table of n till x iteration
+// call fn 3 times
